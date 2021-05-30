3c518dd9a6a3033c9d82aaff0126fc567cbf25b2
Cyprien-Gmd <84139302+Cyprien-Gmd@users.noreply.github.com>
2021-05-30 15:34:00 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,31 @@
+<header>
+
+    <!-- NAV DESKTOP -->
+    <nav>
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="products.php">PRODUCTS</a></li>
+            <li><a href="blog.php">BLOG</a></li>
+            <li><a href="contact.php">CONTACT</a></li>
+        </ul>
+    </nav>
+
+    <!-- MENU BURGER -->
+    <div class="topnav">
+
+
+        <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+
+</header>
+
+<!-- Navigation links (hidden by default) -->
+<div id="myLinks">
+    <a href="index.php">HOME</a>
+    <a href="products.php">PRODUCTS</a>
+    <a href="blog.php">BLOG</a>
+    <a href="contact.php">CONTACT</a>
+</div>
